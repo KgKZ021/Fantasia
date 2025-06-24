28a7572d8071e22e1544eb4e0cd904f44aa4f602
KgKZ021 <kaungkhantzaw011@gmail.com>
2025-06-25 00:53:30 +0200

    feat: Enemy spawning system

---
+++ Assets/Scripts/Monster/MonsterSpawner.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    
+    [System.Serializable]
+    public class Wave
+    {
+        public string waveName;
+        public List<MonsterGroup> monsterGroup;
+        public int waveQuota;//total number of enemies to spawn in this wave
+        public float spawnInterval;
+        public int spawnCount;//number of enemies already spawned in this wave
+    }
+
+    [System.Serializable]
+    public class MonsterGroup
+    {
+        public string monsterName;
+        public int monsterCount;//number of enemies spawned in this wave
+        public int spawnCount;//numbr of enemies of this type already spawned in this wave
+        public GameObject monsterPrefab;
+    }
+
+    [SerializeField] private List<Wave> waves;
+    [SerializeField] private int currentWaveCount; //index of current wave
+
+    [Header("Spawner Attributes")]
+    private float spawnTimer;
+    [SerializeField] private int maxMonstersAllowed;
+    [SerializeField] private int monstersAlive;
+    [SerializeField] private bool maxMonstersReached = false;
+    [SerializeField] private float waveInterval;
+
+    [Header("Spawned Positions")]
+    [SerializeField] public List<Transform> relativeSpawnPoints;
+
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerStats>().transform;
+        CalculateWaveQuota();
+    }
+
+    private void Update()
+    {
+        if (currentWaveCount < waves.Count && waves[currentWaveCount].spawnCount == 0)
+        {
+            StartCoroutine(BeginNextWave());
+        }
+
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= waves[currentWaveCount].spawnInterval)
+        {
+            spawnTimer = 0f;
+            SpawnMonsters();
+        }
+    }
+
+    IEnumerator BeginNextWave()
+    {
+        yield return new WaitForSeconds(waveInterval);
+
+        if(currentWaveCount<waves.Count-1)
+        {
+            currentWaveCount++;
+            CalculateWaveQuota();
+        }
+    }
+
+    private void CalculateWaveQuota()
+    {
+        int currentWaveQuota = 0;
+        foreach (var monsterGroup in waves[currentWaveCount].monsterGroup)
+        {
+            currentWaveQuota += monsterGroup.monsterCount;
+        }
+
+        waves[currentWaveCount].waveQuota = currentWaveQuota;
+        Debug.LogWarning(currentWaveQuota);
+    }
+
+
+/// <summary>
+/// This method will stop spawning if the amount of enem,ies on the map is max.
+/// The method will only spawn enemies in particular waves until it is time for the next waves's enemies to be spawned
+/// </summary>
+    private void SpawnMonsters()
+    {
+        //Check if min number of enemies in the wave have been spawned
+        if (waves[currentWaveCount].spawnCount < waves[currentWaveCount].waveQuota && !maxMonstersReached)
+        {
+            //Spawned each type of enemy till the quota is filled
+            foreach (var monsterGroup in waves[currentWaveCount].monsterGroup)
+            {
+                //Check if the min number of enemies of thistype has been spawned
+                if(monsterGroup.spawnCount < monsterGroup.monsterCount)
+                {
+                    if(monstersAlive >= maxMonstersAllowed)
+                    {
+                        maxMonstersReached = true;
+                        return;
+                    }
+
+                    Instantiate(monsterGroup.monsterPrefab, player.position + relativeSpawnPoints[Random.Range(0,relativeSpawnPoints.Count)].position,Quaternion.identity);
+
+                    monsterGroup.spawnCount++;
+                    waves[currentWaveCount ].spawnCount++;
+                    monstersAlive++;
+                }
+            }
+        }
+        if(monstersAlive < maxMonstersAllowed)
+        {
+            maxMonstersReached =  false;
+        }
+    }
+
+    //Call when an enemy is killed
+    public void OnMonsterKilled()
+    {
+        monstersAlive--;
+    }
+}
--- Assets/Scripts/Monster/MonsterStats.cs
+++ Assets/Scripts/Monster/MonsterStats.cs
@@ -11,11 +11,32 @@ public class MonsterStats : MonoBehaviour
     [HideInInspector] public float currentHealth;
     [HideInInspector] public float currentDamage;
 
+    public float despawnDistance = 50f;
+    Transform player;
+
     private void Awake()
     {
         currentMoveSpeed = monsterSO.MoveSpeed;
         currentHealth = monsterSO.MaxHealth;
         currentDamage = monsterSO.Damage;
+    }
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerStats>().transform;
+    }
+
+    private void Update()
+    {
+        Vector3 flatMonster = new Vector3(transform.position.x, 0f, transform.position.z);
+        Vector3 flatPlayer = new Vector3(player.position.x, 0f, player.position.z);
+        float distance = Vector3.Distance(flatMonster, flatPlayer);
+
+        Debug.Log(distance);
+        if (distance >= despawnDistance)
+        {
+            ReturnMonster();
+        }
     }
 
     public void TakeDamage(float damage)
@@ -40,5 +61,18 @@ public class MonsterStats : MonoBehaviour
             PlayerStats player = collision.gameObject.GetComponent<PlayerStats>();
             player.TakeDamage(currentDamage);
         }
+    }
+
+    private void OnDestroy()
+    {
+        MonsterSpawner monsterSpawner = FindObjectOfType<MonsterSpawner>();
+        monsterSpawner.OnMonsterKilled();
+    }
+
+    //return monster to a spawn positon if the player is too far away
+    private void ReturnMonster()
+    {
+        MonsterSpawner monsterSpawner = FindObjectOfType<MonsterSpawner>();
+        transform.position = player.position + monsterSpawner.relativeSpawnPoints[Random.Range(0, monsterSpawner.relativeSpawnPoints.Count)].position;
     }
 }
